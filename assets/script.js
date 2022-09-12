468af9c06729e88a65c2291f64ad437fb6d9c8d0
Mntnbassfan <108299929+Mntnbassfan@users.noreply.github.com>
2022-09-12 00:48:34 -0600

    added timer function and array of questions / answers

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,49 @@
-document.getElementById("start").addEventListener("click", startQuiz);
-//  -- start quiz
+document.getElementById("start").onclick = setTime;
+{
+  console.log("clicked");
+}
 
+// variable of seconds for timer
 var secondsLeft = 75;
-var numberQuestions = 5,
-  index = 0;
+
+// variable for score
+var score = 0;
+
+// array of questions and correct answers
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title: "Arrays in Javascript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    title:
+      "A very useful tool for used during development and debugging for printing content to the debugger is:",
+    choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+    answer: "console log",
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes",
+  },
+];
 
 //  -- Function for quiz
 function startQuiz() {
@@ -28,12 +68,12 @@ function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function () {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left.";
+    time.textContent = secondsLeft + " seconds left.";
 
     if (secondsLeft <= 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
-      // Calls function to create and append image
+
       //ENDS GAME
       sendMessage();
     }
