11c08d84dcd5e920fec194af94b3806458c96281
Mntnbassfan <108299929+Mntnbassfan@users.noreply.github.com>
2022-09-13 21:58:07 -0600

    fixed errors

--- assets/script.js
+++ assets/script.js
@@ -42,7 +42,7 @@ var questions = [
   {
     currentQuestion:
       "String values must be enclosed within ____ when being assigned to variables.",
-    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
     answer: "quotes",
   },
 ];
@@ -52,45 +52,14 @@ function startQuiz() {
   console.log("startQuiz test");
   setTime();
   displayQuestion();
-
-  //  -- clear container
-
-  //  -- loop through array of questions / answers
-  //   for (i = 0; i < questions.length; i++) {
-  //     console.log(i);
-  //     //      -- add value of question from array to displayedQuestion
-  //     var displayedQuestion = questions[quizIndex].currentQuestion;
-  //     console.log(displayedQuestion);
-
-  //     //   -- show question
-  //     document.getElementById("mainContainer").innerHTML = displayedQuestion;
-
-  //     //  -- create 4 buttons and add answer text to them
-  //     for (var j = 0; j < 5; j++) {
-  //       var displayedAnswer = document.createElement("button");
-  //       displayedAnswer.textContent = questions[j];
-  //       displayedAnswer.addEventListener("click", userChoice);
-
-  //       //  -- wait for click
-  //       //  -- check for correct answer
-  //       function userChoice() {
-  //         if (e.target.button !== questions[quizIndex].answer) {
-  //           //   -- if subtract 10 seconds from time
-  //           //  display "incorrect" in window
-  //         } else {
-  //           //  -- add 10 points to score
-  //           // display "correct" in window
-  //         }
-  //       }
-  //       //  -- check to see if either questions have been completed or timer is 0
-  //       //  -- if not repeat
-  //     }
-  //   }
 }
 function displayQuestion() {
+  // display text of current question
   document.getElementById("mainContainer").innerHTML = "";
   var displayedQuestion = questions[quizIndex].currentQuestion;
   document.getElementById("mainContainer").innerHTML = displayedQuestion;
+
+  //   create buttons and add answer text
   for (i = 0; i < questions[quizIndex].choices.length; i++) {
     var button = document.createElement("button");
     button.innerText = questions[quizIndex].choices[i];
@@ -99,15 +68,18 @@ function displayQuestion() {
   }
 }
 function checkAnswer(event) {
+  // check for right answer
   userChoice = event.target.innerText;
   if (userChoice === questions[quizIndex].answer) {
     quizIndex++;
-    displayQuestion();
+    score = score + 10;
+    // check for wrong answer
   } else {
     quizIndex++;
     secondsLeft = secondsLeft - 10;
-    displayQuestion();
   }
+  document.getElementById("displayScore").textContent =
+    "Current Score: " + score;
 }
 //  -- function for timer
 function setTime() {
